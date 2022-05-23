163eecdf6efaf567b9e291658fcbf4fe80eaae49
Jamie Taffurelli <jltaffurelli@outlook.com>
2022-05-24 00:09:50 +0100

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "vm_backup_policy_id" {
   value       = azurerm_backup_policy_vm.backup.id
   description = "Resource ID of the VM backup policy"
 }
+
+output "sql_backup_policy_id" {
+  value       = jsondecode(azurerm_resource_group_template_deployment.backup.output_content).resourceID.value
+  description = "Resource ID of the sql backup policy"
+}
